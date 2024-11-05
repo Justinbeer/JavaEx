eb1ac1214b4e52af23f9c493c6cd99c44358eea6
Justinbeer <quietly47@gmail.com>
2024-11-05 14:13:53 +0900

    enum

--- src/com/javaex/basic/reftypes/EnumEx.java
+++ src/com/javaex/basic/reftypes/EnumEx.java
@@ -4,10 +4,66 @@ public class EnumEx {
 
 
 	public static void main(String[] args) {
+//		usingEnum();
 		// switchPractice()의 enum 버전
-		
-		
+		switchEnumPractice();
 
+	}
+	
+	private static void usingEnum() {
+		Week today = Week.TUESDAY;
+		System.out.printf("Today is %s(%d)%n", today.name(), today.ordinal());
+		
+		String dayStr = "FRIDAY";
+		// String -> 열거 상수 Convert
+		Week obj = Week.valueOf(dayStr);
+		System.out.printf(obj.name() + " " + obj.ordinal());
+		
+		// 열거 상수 목록
+		System.out.println("Enum Week : ");
+		Week [] days = Week.values();
+		
+		for (int i = 0; i < days.length; i++) {
+			Week day = days[i];
+			System.out.printf("%s -> %d%n", day.name(), day.ordinal());
+		}
+		
+		// compareTo : 두 객체 사이의 순서를 비교하는 메서드
+		Week oterDay = Week.FRIDAY;
+		// THUSDAY와 FRIDAY의 순번 비교
+		System.out.println(today.compareTo(oterDay));
+		
+		
+	}
+	
+	private static void switchEnumPractice() {
+		// Conditional Ex :: switchPractice의 enum 버전
+		Week day = Week.TUESDAY;
+		String action;
+		
+		switch (day) {
+		case SUNDAY:
+			action = "휴식";
+			break;
+		case MONDAY:
+		case TUESDAY:
+		case WEDNESDAY:
+		case THURSDAY:
+			action = "열공";
+			break;
+		case FRIDAY:
+			action = "열공 후 불금";
+			break;
+		case SATURDAY:
+			action = "주말";
+			break;
+		default :
+				action = "난 누구지?";
+				break;
+
+		}
+		System.out.println(day.name() + "에는 " + action);
+		
 	}
 
 }

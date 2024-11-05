da3794ce558ad102a2203a86a61324fbcd0bc668
Justinbeer <quietly47@gmail.com>
2024-11-05 11:57:41 +0900

    String의 포맷

--- src/com/javaex/basic/reftypes/stringEx.java
+++ src/com/javaex/basic/reftypes/stringEx.java
@@ -3,8 +3,8 @@ package com.javaex.basic.reftypes;
 public class stringEx {
 
 	public static void main(String[] args) {
-		
-		usingString();
+//		usingString();
+		stringFormatEx();
 	}
 
 	private static void usingString() {
@@ -21,8 +21,34 @@ public class stringEx {
 		// 객체의 경우, 값의 비교는 equals 메서드
 		System.out.println(str.equals(str3));
 		
+	}
+	
+	private static void stringFormatEx() {
+		// 포매팅 문자, %s(문자열), %d(정수), %n(개행)
+		// 10개의 사과중에 3개를 먹었다
 		
+		int total = 10, eat = 3;
+		String fruit = "사과";
+		System.out.println(total + "개의 " + fruit + "중에 " + eat + "개를 먹었다");
+		
+		// -> formatting
+		System.out.printf("%d개의 %s중에 %d개를 먹었다%n", total, fruit, eat);
+		
+		// String.format 메서드 활용한 것
+		String template = "%d개의 %s중에 %d개를 먹었다";
+		System.out.println(String.format(template, 10, "사과", 5));
+		System.out.println(String.format(template, 5, "바나나", 2));
+		
+		// 천단위에, 포매팅
+		int amount = 123_456_789;
+		System.out.printf("현재 잔고는 : %d입니다.%n", amount);
+		System.out.printf("현재 잔고는 : %,d입니다.%n", amount);
+		
+		// %f
+		float interestRate = 1.2345f;
+		System.out.printf("현재 이자율은 %.2f입니다.%n", interestRate);
 		
 	}
 	
+
 }
